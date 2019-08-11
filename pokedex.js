4476ca4955b69b1240d647c36be2885fbc28f255
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-11 19:35:01 +0200

    Pas encore fonctionnel...

---
+++ pokedex.js
@@ -0,0 +1,33 @@
+const pokedex ='https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+const pokemons = [];
+
+fetch(pokedex)
+  .then(blob => blob.json())
+  .then(data => pokemons.push(...data.pokemon));
+
+function findPokemon(searchPokemon, pokemons){
+  return pokemons.filter(pokemon => {
+    const regex = new RegExp(searchPokemon, 'gi');
+    pokemon.name.match(regex);
+  });
+}
+
+function showResult() {
+  const arrayResults = findPokemon(this.value, pokemons);
+  const html = arrayResults.map(pokemon => {
+    return `
+    <li>
+    <img src='${pokemon.img}' width='42' height='42'>
+    <span>${pokemon.name}</span>
+    <span>${pokemon.width}</span>
+    </li>
+    `;
+  }).join('');
+  result.innerHTML = html ;
+}
+
+const input = document.querySelector('input');
+const result = document.querySelector('ul');
+
+input.addEventListener('change', showResult);
+input.addEventListener('keyup', showResult);
